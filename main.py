93fa00e4a2485571ac5f0b80d4ba09d3e1ff4476
ben stear <bjs385@drexel.edu>
2020-02-20 12:13:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main():
 
     my_output = str(my_input)
 
-    print(f"::set-output name=myOUTPUTt::{my_output}")
-
-
+    #print(f"::set-output name=myOUTPUT::{my_output}")
+    print(f"www.github.com/{my_input}")
+    
 if __name__ == "__main__":
     main()
