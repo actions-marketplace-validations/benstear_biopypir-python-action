696d42bf514950f15149e5d4b3a0a41182b52959
ben stear <bjs385@drexel.edu>
2020-03-05 13:33:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import os
 
 
 def main():
+    '''
     repo_name = os.environ["INPUT_MYREPO"]
     python_version = os.environ["INPUT_PYTHON_VERSION"]
     os = os.environ["INPUT_OS"]
     lint = os.environ["INPUT_LINT"]
     package_name = os.environ["INPUT_PACKAGE_NAME"]
+    '''
+    os.system('echo $PWD')
+    os.system('ls -a')
     
     #my_output = str(my_input)
-
     #print(f"::set-output name=myOUTPUT::{my_output}")
-    print(f"www.github.com/{my_input}")
+    #print(f"www.github.com/{my_input}")
     
 if __name__ == "__main__":
     main()
