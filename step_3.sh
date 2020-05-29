d2d00ec075ed20cbf4f2c971f75610c030d388b9
ben stear <bjs385@drexel.edu>
2020-05-29 15:50:46 -0400

    Update step_3.sh

--- step_3.sh
+++ step_3.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 pip install --upgrade pip setuptools wheel
-pip install --no-binary :all: nmslib
-pip install .
-pip install python-coveralls
+pip install os
+pip install time
+pip install json
+pip install pandas 
+pip install tabulate 
+pip install glob
+pip install numpy 
+python3 process_logs.py
+
