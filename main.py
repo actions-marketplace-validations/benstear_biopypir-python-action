d5810a1494a13b504e97c7394cbd2aaa7d478c3c
ben stear <bjs385@drexel.edu>
2020-05-29 15:54:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,62 @@ import os
 #import pandas  # noqa We are just importing this to prove the dependency installed correctly
 
 
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 15:09:37 2020
+
+@author: stearb
+"""
+
+# read biopypir_log
+# get name date, run id, and info
+# and check if anything is different, only change 'last checked' if everything same   
+import os
+import time
+import json
+import pandas as pd
+
+import tabulate as tb
+
+import glob
+import numpy as np
+
+local_path= '/Users/stearb/desktop/temp_biop/logs/*.json'
+logs_path = "logs/*.json"
+
+
+all_logs = glob.glob(logs_path) 
+#all_logs = glob.glob(local_path) 
+print('Total Logs found: '+str(len(all_logs)))
+
+new = 1
+
+if new:
+    for i in range(0,1): #len(all_logs)):
+        with open(all_logs[i]) as f:
+                data = json.load(f)
+
+#log1 =  json.loads(data)
+    
+
+#def create_table(log_path_list):
+    
+table = pd.DataFrame([data['Package'],'Gold','TaylorLab'])#,columns=['Package','Badge'])
+    
+    
+
+'''
 def main():
-    '''
+    
     repo_name = os.environ["INPUT_MYREPO"]
     python_version = os.environ["INPUT_PYTHON_VERSION"]
     os = os.environ["INPUT_OS"]
     lint = os.environ["INPUT_LINT"]
     package_name = os.environ["INPUT_PACKAGE_NAME"]
-    '''
+    
     
     print(os.system('echo $PWD'))
     
@@ -18,7 +66,8 @@ def main():
     #my_output = str(my_input)
     #print(f"::set-output name=myOUTPUT::{my_output}")
     #print(f"www.github.com/{my_input}")
-    
+'''
+
 if __name__ == "__main__":
     main()
     
