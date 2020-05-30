63aa13825c0e19c1959d7a2e9f513d76f56743c6
ben stear <bjs385@drexel.edu>
2020-05-30 10:01:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ if new:
 df = pd.DataFrame([data['Package'],'Gold','TaylorLab'])#,columns=['Package','Badge'])
 df.to_csv('table1.csv',sep='\t')  
 
-print("::set-output name=table_output::df")
+print("::set-output name=table_output::{df}")
 
 '''
 def main():
@@ -61,9 +61,10 @@ def main():
     
     #my_output = str(my_input)
     #print(f"::set-output name=myOUTPUT::{my_output}")
-    #print(f"www.github.com/{my_input}")
+    print(f"www.github.com/{my_input}")
 '''
 
 if __name__ == "__main__":
     main()
+    print("www.github.com/benstear")
     
